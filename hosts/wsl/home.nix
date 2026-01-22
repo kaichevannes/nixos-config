65ea95adb936a189807a81f28c12d5ffbc42bbe1
Kai Chevannes <chevannes.kai@gmail.com>
2026-01-22 03:20:21 +0000

    feat: add .managed_known_hosts for permanent known_hosts like github
    Instead of having to put just github in every users known_hosts file.
    any others are likely just temporary and I can just accept them one off
    with ssh. The reason I want the github one there is so that in LazyGit
    it doesn't break on the first push, meaning I would have to quit out and
    do one git push manually every time I fresh install.

--- hosts/wsl/home.nix
+++ hosts/wsl/home.nix
@@ -12,19 +12,8 @@
     xclip
   ];
 
-  home.file = {
-    ".ssh/known_hosts" = {
-      source = ./known_hosts;
-      force = true;
-    };
-  };
-
   home.activation.copyWeztermConfigToWindows = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      if [ "$(uname -r | sed -n 's/.*\( *Microsoft *\).*/\1/ip')" ]; then
-        WIN_HOME="/mnt/c/Users/$(whoami)"
-        if [ -d "$WIN_HOME" ]; then
-          cp "$HOME/nixos-config/hosts/wsl/wezterm.lua" "$WIN_HOME/.wezterm.lua"
-        fi
-      fi
+      WIN_HOME="/mnt/c/Users/$(whoami)"
+      cp "$HOME/nixos-config/hosts/wsl/wezterm.lua" "$WIN_HOME/.wezterm.lua"
   '';
 }
