fcffcfd6b92e187819d9af36c56642182452f2da
Kai Chevannes <chevannes.kai@gmail.com>
2026-01-22 02:08:25 +0000

    feat: move ssh config into matchBlocks

--- home/ssh.nix
+++ home/ssh.nix
@@ -3,13 +3,19 @@
 
   programs.ssh = {
     enable = true;
-    extraConfig = ''
-      AddKeysToAgent yes
-      IdentityFile ~/.ssh/id_ed25519
-    '';
+    enableDefaultConfig = false;
+    matchBlocks."*" = {
+      forwardAgent = false;
+      addKeysToAgent = "yes";
+      compression = false;
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+      identityFile = "~/.ssh/id_ed25519";
+    }; 
   };
-
-  home.file.".ssh/known_hosts".text = ''
-    github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
-  '';
 }
