15898bdf8c12356592255e6309c0612e34bab35d
Kai Chevannes <chevannes.kai@gmail.com>
2026-01-22 04:08:30 +0000

    feat: refer dynamically to the wezterm.lua file location

--- hosts/wsl/home.nix
+++ hosts/wsl/home.nix
@@ -14,6 +14,6 @@
 
   home.activation.copyWeztermConfigToWindows = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       WIN_HOME="/mnt/c/Users/$(whoami)"
-      cp "$HOME/nixos-config/hosts/wsl/wezterm.lua" "$WIN_HOME/.wezterm.lua"
+      cp "${builtins.toString ./wezterm.lua}" "$WIN_HOME/.wezterm.lua"
   '';
 }
