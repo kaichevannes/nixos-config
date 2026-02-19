7cc8c511b493193eaa77d5576308c153b7d4bc0c
Kai Chevannes <chevannes.kai@gmail.com>
2026-02-19 23:17:38 +0000

    feat: set foot to be hyprland terminal

--- aspects/desktop/hyprland.nix
+++ aspects/desktop/hyprland.nix
@@ -6,4 +6,19 @@
         enable = true;
       };
     };
+
+  homeManager =
+    { pkgs, ... }:
+    {
+      wayland.windowManager.hyprland = {
+        enable = true;
+        settings = {
+          "$terminal" = "foot";
+        };
+      };
+
+      home.packages = with pkgs; [
+        wayland
+      ];
+    };
 }
