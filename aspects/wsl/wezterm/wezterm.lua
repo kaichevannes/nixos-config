ca778d375c3253e75dde3ba744139a8a561a31ad
Kai Chevannes <chevannes.kai@gmail.com>
2026-02-19 04:10:48 +0000

    feat: set default wezterm wsl domain to NixOS

--- aspects/wsl/wezterm/wezterm.lua
+++ aspects/wsl/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-    config.default_domain = 'WSL:Ubuntu'
+    config.default_domain = 'WSL:NixOS'
 end
 
 config.color_scheme = "SleepyHollow"
