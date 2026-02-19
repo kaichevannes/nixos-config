d33765ff52f16605f91ecc8b4bea0ba18d85a3a4
Kai Chevannes <chevannes.kai@gmail.com>
2026-02-19 03:38:44 +0000

    feat: move user.nix to cheva aspect

--- aspects/cheva/default.nix
+++ aspects/cheva/default.nix
@@ -1,8 +1,28 @@
 {
-  homeManager = {
-    programs.git.settings = {
-      user.name = "Kai Chevannes";
-      user.email = "chevannes.kai@gmail.com";
+  nixos =
+    { config, pkgs, ... }:
+    {
+      programs.zsh.enable = true;
+      users = {
+        defaultUserShell = pkgs.zsh;
+
+        users.cheva = {
+          isNormalUser = true;
+          description = "Kai Chevannes";
+          extraGroups = [
+            "networkmanager"
+            "wheel"
+          ];
+        };
+      };
     };
-  };
+
+  homeManager =
+    { ... }:
+    {
+      programs.git.settings = {
+        user.name = "Kai Chevannes";
+        user.email = "chevannes.kai@gmail.com";
+      };
+    };
 }
