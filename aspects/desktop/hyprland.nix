e1867db1e0a1fbbec0fa0bcf05e7e1c77c1eed01
Kai Chevannes <chevannes.kai@gmail.com>
2026-02-20 01:32:02 +0000

    feat: add basic hyprland config

--- aspects/desktop/hyprland.nix
+++ aspects/desktop/hyprland.nix
@@ -8,16 +8,26 @@
     };
 
   homeManager =
-    { pkgs, ... }:
+    { config, pkgs, ... }:
     {
       wayland.windowManager.hyprland = {
         enable = true;
         settings = {
           "$mod" = "SUPER";
           "$terminal" = "foot";
+          "$fileManager" = "yazi";
+          "$browser" = "firefox";
+
+          exec-once = [
+            "vicinae server"
+          ];
 
           bind = [
-            "$mod, Q, exec, $terminal"
+            "$mod, T, exec, $terminal"
+            "$mod, B, exec, $browser"
+            "$mod, F, exec, $fileManager"
+            "$mod, D, exec, vicinae toggle"
+            "$mod, Q, killactive"
             "$mod, M, exit"
           ];
 
@@ -27,8 +37,52 @@
 
           misc = {
             disable_hyprland_logo = true;
+            disable_splash_rendering = true;
+          };
+
+          general = {
+            gaps_in = 5;
+            gaps_out = 5;
+
+            border_size = 2;
+          };
+
+          decoration = {
+            rounding = 10;
           };
         };
+        extraConfig = ''
+          layerrule {
+            name = vicinae-blur
+            blur = on
+            ignore_alpha = 0
+            match:namespace = vicinae
+          }
+
+          layerrule {
+            name = vicinae-no-animation
+            no_anim = on
+            match:namespace = vicinae
+          }
+        '';
+      };
+
+      services.vicinae = {
+        enable = true;
+        package = pkgs.vicinae;
+        # extensions = [
+        #   (config.lib.vicinae.mkExtension {
+        #     name = "nix";
+        #     src =
+        #       pkgs.fetchFromGithub {
+        #         owner = "vicinaehq";
+        #         repo = "vicinae-extensions";
+        #         rev = "cf30b80f619282d45b1748eb76e784a4f875bb01";
+        #         sha256 = "";
+        #       }
+        #       + "/extensions/nix";
+        #   })
+        # ];
       };
     };
 }
