4f15a40817f97dac303bcbf3861b88da9bd054d6
Kai Chevannes <chevannes.kai@gmail.com>
2026-02-22 02:15:53 +0000

    feat(hyprland): default workspaces

--- aspects/desktop/hyprland.nix
+++ aspects/desktop/hyprland.nix
@@ -21,10 +21,57 @@
             "desc:ASUSTek COMPUTER INC VG27A M3LMQS265113, 2560x1440@144, 0x0, 1"
           ];
 
+          exec-once = [
+            "[workspace 1 silent] foot"
+            "[workspace 2 silent] firefox"
+            "[workspace 5 silent] MOZ_ENABLE_WAYLAND=0 firefox -P focumon https://focumon.com --kiosk"
+          ];
+
           bind = [
             "$mod, T, exec, $terminal"
             "$mod, B, exec, $browser"
             "$mod, D, exec, vicinae toggle"
+
+            "$mod, 1, workspace, 1"
+            "$mod, 2, workspace, 2"
+            "$mod, 3, workspace, 3"
+            "$mod, 4, workspace, 4"
+            "$mod, 5, workspace, 5"
+            "$mod, 6, workspace, 6"
+            "$mod, 7, workspace, 7"
+            "$mod, 8, workspace, 8"
+            "$mod, 9, workspace, 9"
+            "$mod, 0, workspace, 10"
+
+            "$mod, !, movetoworkspace, 1"
+            "$mod, [, movetoworkspace, 2"
+            "$mod, {, movetoworkspace, 3"
+            "$mod, (, movetoworkspace, 4"
+            "$mod, +, movetoworkspace, 5"
+            "$mod, *, movetoworkspace, 6"
+            "$mod, ), movetoworkspace, 7"
+            "$mod, }, movetoworkspace, 8"
+            "$mod, ], movetoworkspace, 9"
+            "$mod, #, movetoworkspace, 10"
+
+            "$mod, h, movefocus, l"
+            "$mod, j, movefocus, d"
+            "$mod, k, movefocus, u"
+            "$mod, l, movefocus, r"
+            "$mod, left, movefocus, l"
+            "$mod, down, movefocus, d"
+            "$mod, up, movefocus, u"
+            "$mod, right, movefocus, r"
+
+            "$mod+Shift, h, swapwindow, l"
+            "$mod+Shift, j, swapwindow, d"
+            "$mod+Shift, k, swapwindow, u"
+            "$mod+Shift, l, swapwindow, r"
+            "$mod+Shift, left, swapwindow, l"
+            "$mod+Shift, down, swapwindow, d"
+            "$mod+Shift, up, swapwindow, u"
+            "$mod+Shift, right, swapwindow, r"
+
             "$mod, Q, killactive"
             "$mod, M, exit"
           ];
