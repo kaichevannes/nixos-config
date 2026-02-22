ec9ca11c8bccc57b02dcc8a0f1c47ee0e7fefde2
Kai Chevannes <chevannes.kai@gmail.com>
2026-02-22 02:17:05 +0000

    feat: reduce vicinae settings to essentials

--- aspects/desktop/vicinae.nix
+++ aspects/desktop/vicinae.nix
@@ -10,28 +10,8 @@
           autoStart = true;
         };
         settings = {
-          pop_to_root_on_close = true;
           escape_key_behaviour = "close_window";
-          font = {
-            normal = {
-              family = "auto";
-              size = 12;
-            };
-          };
         };
-        extensions = [
-          (config.lib.vicinae.mkExtension {
-            name = "nix";
-            src =
-              pkgs.fetchFromGitHub {
-                owner = "vicinaehq";
-                repo = "extensions";
-                rev = "cf30b80f619282d45b1748eb76e784a4f875bb01";
-                sha256 = "sha256-KwNv+THKbNUey10q26NZPDMSzYTObRHaSDr81QP9CPY=";
-              }
-              + "/extensions/nix";
-          })
-        ];
       };
 
       wayland.windowManager.hyprland = {
