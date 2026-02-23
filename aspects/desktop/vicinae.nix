21b6080651529c2faeb0dd24f35f3db4a1524c6d
Kai Chevannes <chevannes.kai@gmail.com>
2026-02-23 15:52:47 +0000

    feat: remove unused extensions from vicinae

--- aspects/desktop/vicinae.nix
+++ aspects/desktop/vicinae.nix
@@ -11,7 +11,50 @@
         };
         settings = {
           escape_key_behaviour = "close_window";
+
+          font = {
+            normal = {
+              family = "auto";
+              size = 12;
+            };
+          };
+
+          launcher_window = {
+            compact_mode.enabled = true;
+          };
+
+          favorites = [ ];
+
+          providers = {
+            browser-extensions.enabled = false;
+            core.enabled = false;
+            clipboard.enabled = false;
+            developer.enabled = false;
+            files.enabled = false;
+            font.enabled = false;
+            manage-shortcuts.enabled = false;
+            power.enabled = false;
+            raycast-compact.enabled = false;
+            system.enabled = false;
+            theme.enabled = false;
+            wm.enabled = false;
+
+            applications.enabled = true;
+          };
         };
+        extensions = [
+          (config.lib.vicinae.mkExtension {
+            name = "nix";
+            src =
+              pkgs.fetchFromGitHub {
+                owner = "vicinaehq";
+                repo = "extensions";
+                rev = "cf30b80f619282d45b1748eb76e784a4f875bb01";
+                sha256 = "sha256-KwNv+THKbNUey10q26NZPDMSzYTObRHaSDr81QP9CPY=";
+              }
+              + "/extensions/nix";
+          })
+        ];
       };
 
       wayland.windowManager.hyprland = {
