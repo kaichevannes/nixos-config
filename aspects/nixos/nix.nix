98a250eb399dd28914031c057cf32bc17a076fa1
Kai Chevannes <chevannes.kai@gmail.com>
2026-02-23 22:49:17 +0000

    feat: add nix optimizaitons

--- aspects/nixos/nix.nix
+++ aspects/nixos/nix.nix
@@ -1,7 +1,39 @@
 {
   nixos =
-    { ... }:
+    { user, inputs, ... }:
     {
       nixpkgs.config.allowUnfree = true;
+
+      # https://github.com/anotherhadi/nixy/blob/main/nixos/nix.nix#L24-L54
+      nix = {
+        nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
+        channel.enable = false;
+        extraOptions = ''
+          warn-dirty = false
+        '';
+        settings = {
+          download-buffer-size = 262144000; # 250 MB (250 * 1024 * 1024)
+          auto-optimise-store = true;
+          substituters = [
+            # high priority since it's almost always used
+            "https://cache.nixos.org?priority=10"
+
+            "https://hyprland.cachix.org"
+            "https://nix-community.cachix.org"
+            "https://numtide.cachix.org"
+          ];
+          trusted-public-keys = [
+            "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+          ];
+        };
+        gc = {
+          automatic = true;
+          persistent = true;
+          dates = "weekly";
+          options = "--delete-older-than 7d";
+        };
+      };
     };
 }
