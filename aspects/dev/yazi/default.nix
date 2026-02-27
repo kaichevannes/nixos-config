085200123228ffc90b49da3311c0f448e8bb1e51
Kai Chevannes <chevannes.kai@gmail.com>
2026-02-27 04:34:08 +0000

    feat(yazi): add icon overrides

--- aspects/dev/yazi/default.nix
+++ aspects/dev/yazi/default.nix
@@ -6,7 +6,42 @@
         enable = true;
         enableZshIntegration = true;
         shellWrapperName = "y";
-        theme.mgr.syntect_theme = ./gruvbox-material.tmTheme;
+        theme = {
+          mgr.syntect_theme = ./gruvbox-material.tmTheme;
+          # https://github.com/sxyazi/yazi/blob/884c265de1b4a05c96233fe1d4c37955ff0c032e/yazi-config/preset/theme-dark.toml#L682
+          icon.prepend_exts = [
+            {
+              name = "html";
+              text = "";
+              fg = "#e44d26";
+            }
+            {
+              name = "js";
+              text = "";
+              fg = "#cbcb41";
+            }
+            {
+              name = "svg";
+              text = "";
+              fg = "#ffb13b";
+            }
+            {
+              name = "mp3";
+              text = "󰝚";
+              fg = "#00afff";
+            }
+            {
+              name = "mp4";
+              text = "";
+              fg = "#fd971f";
+            }
+            {
+              name = "zip";
+              text = "󰛫";
+              fg = "#eca517";
+            }
+          ];
+        };
       };
     };
 }
