d0e107d4216baf12938e831fdd41c4791bb598c8
Kai Chevannes <chevannes.kai@gmail.com>
2026-02-27 00:09:33 +0000

    feat: add helix master

--- aspects/dev/helix/default.nix
+++ aspects/dev/helix/default.nix
@@ -1,10 +1,16 @@
 {
   homeManager =
-    { pkgs, ... }:
+    {
+      inputs,
+      pkgs,
+      ...
+    }:
     {
       programs.helix = {
         enable = true;
         defaultEditor = true;
+        # Helix master from flake
+        package = inputs.helix.packages.${pkgs.stdenv.hostPlatform.system}.helix;
         settings = {
           theme = "gruvbox-material-transparent";
           editor = {
--- aspects/nixos/nix.nix
+++ aspects/nixos/nix.nix
@@ -18,14 +18,14 @@
             # high priority since it's almost always used
             "https://cache.nixos.org?priority=10"
 
-            "https://hyprland.cachix.org"
             "https://nix-community.cachix.org"
-            "https://numtide.cachix.org"
+            "https://hyprland.cachix.org"
+            "https://helix.cachix.org"
           ];
           trusted-public-keys = [
             "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-            "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+            "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
           ];
         };
         gc = {
