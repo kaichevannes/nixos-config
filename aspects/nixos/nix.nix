83bd26729366c3f6436020e216477f45bea72144
Kai Chevannes <chevannes.kai@gmail.com>
2026-03-11 17:34:50 +0000

    feat: remove garbage collection. replace in future with impermanance

--- aspects/nixos/nix.nix
+++ aspects/nixos/nix.nix
@@ -26,12 +26,12 @@
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           ];
         };
-        gc = {
-          automatic = true;
-          persistent = true;
-          dates = "weekly";
-          options = "--delete-older-than 7d";
-        };
+        # gc = {
+        #   automatic = true;
+        #   persistent = true;
+        #   dates = "weekly";
+        #   options = "--delete-older-than 7d";
+        # };
       };
     };
 }
