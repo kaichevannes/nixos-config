69ea4df1f939e9a8cca1fc57cdb9c4b9a7b560a5
Kai Chevannes <chevannes.kai@gmail.com>
2026-04-05 15:52:17 +0100

    feat: add declarative user password

--- services/secrets/nixos.nix
+++ services/secrets/nixos.nix
@@ -1,4 +1,8 @@
 { pkgs, user, ... }:
+let
+  ageKeyDir = "/home/${user}/.config/sops/age";
+  ageKeyFile = "${ageKeyDir}/keys.txt";
+in
 {
   imports = [ ../ssh/nixos.nix ];
 
@@ -8,15 +12,16 @@
     wantedBy = [ "graphical-session.target" ];
     serviceConfig = {
       Type = "oneshot";
+      ConditionPathExists = "!${ageKeyFile}";
       ExecStart = [
-        "/run/current-system/sw/bin/mkdir -p %h/.config/sops/age"
-        "/run/current-system/sw/bin/bash -c \"${pkgs.proton-pass-cli}/bin/pass-cli item view --vault-name Keys --item-title id_ed25519 --field private_key | ${pkgs.ssh-to-age}/bin/ssh-to-age -private-key -i - > %h/.config/sops/age/keys.txt\""
+        "/run/current-system/sw/bin/mkdir -p ${ageKeyDir}"
+        "/run/current-system/sw/bin/bash -c \"${pkgs.proton-pass-cli}/bin/pass-cli item view --vault-name Keys --item-title id_ed25519 --field private_key | ${pkgs.ssh-to-age}/bin/ssh-to-age -private-key -i - > ${ageKeyFile}\""
       ];
     };
   };
 
   sops.defaultSopsFile = ../../secrets/secrets.yaml;
-  sops.age.keyFile = "/home/${user}/.config/sops/age/keys.txt";
+  sops.age.keyFile = ageKeyFile;
 
   sops.secrets = (import ../../secrets/manifest.nix { inherit user; });
 }
--- users/cheva.nix
+++ users/cheva.nix
@@ -2,6 +2,8 @@
   nixos =
     { config, pkgs, ... }:
     {
+      sops.secrets.password_cheva.neededForUsers = true;
+
       programs.zsh.enable = true;
       users = {
         defaultUserShell = pkgs.zsh;
@@ -9,6 +11,7 @@
         users.cheva = {
           isNormalUser = true;
           description = "Kai Chevannes";
+          hashedPasswordFile = config.sops.secrets.password_cheva.path;
           extraGroups = [
             "networkmanager"
             "wheel"
