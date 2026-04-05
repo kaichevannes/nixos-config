11800314402845e26b635e213a2e746554293d7b
Kai Chevannes <chevannes.kai@gmail.com>
2026-04-05 19:09:10 +0100

    fix: removing condition fixes startup

--- services/secrets/nixos.nix
+++ services/secrets/nixos.nix
@@ -12,7 +12,6 @@ in
     wantedBy = [ "graphical-session.target" ];
     serviceConfig = {
       Type = "oneshot";
-      ConditionPathExists = "!${ageKeyFile}";
       ExecStart = [
         "/run/current-system/sw/bin/mkdir -p ${ageKeyDir}"
         "/run/current-system/sw/bin/bash -c \"${pkgs.proton-pass-cli}/bin/pass-cli item view --vault-name Keys --item-title id_ed25519 --field private_key | ${pkgs.ssh-to-age}/bin/ssh-to-age -private-key -i - > ${ageKeyFile}\""
