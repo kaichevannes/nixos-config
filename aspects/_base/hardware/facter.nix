ae452c36103c8e18d272080728a5a7a09bf2f80f
Kai Chevannes <chevannes.kai@gmail.com>
2026-04-07 16:26:23 +0100

    docs: info on what facter sets up for us

--- aspects/_base/hardware/facter.nix
+++ aspects/_base/hardware/facter.nix
@@ -1,4 +1,6 @@
 { config, ... }:
 {
+  # Facter sets up: bluetooth, wifi, networking, hostplatform,
+  #     virtualization type, usb, storage devices, etc.
   hardware.facter.reportPath = ../../hosts/${config.meta.hostname}/facter.json;
 }
