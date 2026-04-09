6b4956fae6e843256265744227e7f47c781fd133
Kai Chevannes <chevannes.kai@gmail.com>
2026-04-09 22:17:15 +0100

    feat: test dir

--- install.nix
+++ install.nix
@@ -11,35 +11,30 @@ pkgs.writeShellApplication {
   ];
   text = ''
     readonly HOST="''${1:?Usage: ...#install -- <hostname>}"
-    PERSIST_DIR="/mnt/persist"
+    readonly REPO="nixos-config/disko"
 
-    if nix eval "github:kaichevannes/nixos-config#nixosConfigurations.$HOST.config.disko.devices" >/dev/null 2>&1; then
-      echo "Partitioning disk with disko"
-      disko --flake "github:kaichevannes/nixos-config#$HOST" --mode destroy,format,mount
-    else
-      echo "No disko config found, skipping partitioning"
-      PERSIST_DIR=""
-    fi
+    echo "Partitioning disk with disko"
+    disko --flake "github:kaichevannes/$REPO#$HOST" --mode destroy,format,mount
 
     echo "Cloning config"
-    git clone "https://github.com/kaichevannes/nixos-config.git" "$PERSIST_DIR/etc/nixos"
-    git -C "$PERSIST_DIR/etc/nixos" remote set-url origin "git@github.com:kaichevannes/nixos-config.git"
+    git clone "https://github.com/kaichevannes/$REPO.git" "/mnt/persist/etc/nixos"
+    git -C "/mnt/persist/etc/nixos" remote set-url origin "git@github.com:kaichevannes/$REPO.git"
 
     echo "Initialising facter.json"
-    nixos-facter -o "$PERSIST_DIR/etc/nixos/hosts/$HOST/facter.json"
-    git -C "$PERSIST_DIR/etc/nixos" add "hosts/$HOST/facter.json" "hosts/$HOST/filesystem.nix"
+    nixos-facter -o "/mnt/persist/etc/nixos/hosts/$HOST/facter.json"
+    git -C "/mnt/persist/etc/nixos" add "hosts/$HOST/facter.json" "hosts/$HOST/filesystem.nix"
 
     echo "Initialising age key"
-    mkdir -p "$PERSIST_DIR/var/lib/sops-nix"
+    mkdir -p "/mnt/persist/var/lib/sops-nix"
     pass-cli login --interactive
     pass-cli item view \
       --vault-name Keys \
       --item-title id_ed25519 \
       --field private_key \
-      | ssh-to-age -private-key -i - > "$PERSIST_DIR/var/lib/sops-nix/key.txt"
-    chmod 400 "$PERSIST_DIR/var/lib/sops-nix/key.txt"
+      | ssh-to-age -private-key -i - > "/mnt/persist/var/lib/sops-nix/key.txt"
+    chmod 400 "/mnt/persist/var/lib/sops-nix/key.txt"
 
     echo "Installing NixOS"
-    nixos-install --flake "git+file://$PERSIST_DIR/etc/nixos#$HOST" --no-root-passwd
+    nixos-install --flake "git+file:///mnt/persist/etc/nixos#$HOST" --no-root-passwd
   '';
 }
