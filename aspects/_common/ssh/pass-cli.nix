5f874fdb773d17ecdd062b1a3595e1126a9df747
Kai Chevannes <chevannes.kai@gmail.com>
2026-04-09 19:05:00 +0100

    feat: switch back to fs proton pass to test impermanence

--- aspects/_common/ssh/pass-cli.nix
+++ aspects/_common/ssh/pass-cli.nix
@@ -4,14 +4,14 @@
     proton-pass-cli
   ];
 
-  sops.secrets.proton_pass_encryption_key = { };
-  sops.secrets.proton_pass_personal_access_token = { };
+  # sops.secrets.proton_pass_encryption_key = { };
+  # sops.secrets.proton_pass_personal_access_token = { };
 
-  sops.templates.proton-pass-env.content = ''
-    PROTON_PASS_KEY_PROVIDER=env
-    PROTON_PASS_ENCRYPTION_KEY=${config.sops.placeholder.proton_pass_encryption_key}
-    PROTON_PASS_PERSONAL_ACCESS_TOKEN=${config.sops.placeholder.proton_pass_personal_access_token}
-  '';
+  # sops.templates.proton-pass-env.content = ''
+  #   PROTON_PASS_KEY_PROVIDER=env
+  #   PROTON_PASS_ENCRYPTION_KEY=${config.sops.placeholder.proton_pass_encryption_key}
+  #   PROTON_PASS_PERSONAL_ACCESS_TOKEN=${config.sops.placeholder.proton_pass_personal_access_token}
+  # '';
 
   systemd.user.services.proton-pass-ssh-agent = {
     description = "Proton Pass SSH Agent";
@@ -19,7 +19,7 @@
     wantedBy = [ "graphical-session.target" ];
 
     serviceConfig = {
-      EnvironmentFile = config.sops.templates.proton-pass-env.path;
+      # EnvironmentFile = config.sops.templates.proton-pass-env.path;
       ExecStartPre = "${pkgs.proton-pass-cli}/bin/pass-cli pass-cli login";
       ExecStart = "${pkgs.proton-pass-cli}/bin/pass-cli ssh-agent start";
       Restart = "on-failure";
@@ -28,6 +28,7 @@
   };
 
   environment.sessionVariables = {
+    PROTON_PASS_KEY_PROVIDER = "fs";
     SSH_AUTH_SOCK = "$HOME/.ssh/proton-pass-agent.sock";
   };
 }
