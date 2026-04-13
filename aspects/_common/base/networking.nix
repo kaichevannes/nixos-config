be79bc8c2291d1bc53d7b2a5c71abcbdfd22ff06
Kai Chevannes <chevannes.kai@gmail.com>
2026-04-13 22:48:01 +0100

    feat: add custom networking again to be explicit

--- aspects/_common/base/networking.nix
+++ aspects/_common/base/networking.nix
@@ -1,4 +1,10 @@
 { config, ... }:
 {
   networking.hostName = config.meta.hostname;
+  networking.networkmanager = {
+    enable = true;
+    dhcp = "dhcpcd";
+    dns = "systemd-resolved";
+  };
+  services.resolved.enable = true;
 }
