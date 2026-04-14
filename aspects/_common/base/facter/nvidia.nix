efcc4f2c007561365cf7a170b6bf018ce71808c4
Kai Chevannes <chevannes.kai@gmail.com>
2026-04-14 16:02:57 +0100

    fix: correctly detect nvidia drivers

--- aspects/_common/base/facter/nvidia.nix
+++ aspects/_common/base/facter/nvidia.nix
@@ -1,14 +1,14 @@
 # Referencing https://github.com/NixOS/nixpkgs/blob/68d8aa3d661f0e6bd5862291b5bb263b2a6595c9/nixos/modules/hardware/facter/graphics/amd.nix
 { lib, config, ... }:
 let
-  collectDrivers = list: lib.foldl' (lst: value: lst ++ value.driver_modules or [ ]) [ ] list;
   cfg = config.hardware.facter.detected.graphics.nvidia;
 in
 {
   options.hardware.facter.detected.graphics = {
     nvidia.enable = lib.mkEnableOption "Enable the nvidia graphics module" // {
-      default = builtins.elem "nvidia" (
-        collectDrivers (config.hardware.facter.report.hardware.graphics_card or [ ])
+      # 10de is nvidia vendor hex
+      default = builtins.any (elem: (elem.vendor.hex or "") == "10de") (
+        config.hardware.facter.report.hardware.graphics_card or [ ]
       );
       defaultText = "hardware dependent";
     };
