11fd999bdf891d83aeb24c38399f66837d07baa0
Kai Chevannes <chevannes.kai@gmail.com>
2026-04-15 19:15:12 +0100

    feat: simplify ssh

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -1,11 +1,17 @@
-{ lib, ... }:
+{ config, lib, ... }:
 {
   options.modules.ssh = {
     enable = lib.mkEnableOption "ssh";
   };
 
+  config = lib.mkIf config.modules.ssh.enable {
+    programs.ssh = {
+      knownHosts."github.com".publicKey =
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+    };
+  };
+
   imports = [
-    ./ssh.nix
-    ./pass-cli.nix
+    ./agent.nix
   ];
 }
