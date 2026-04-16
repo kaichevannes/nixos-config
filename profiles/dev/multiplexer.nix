6e7f5845e50549f37c7bc3584e78fcb53a490e0b
Kai Chevannes <chevannes.kai@gmail.com>
2026-04-16 18:51:47 +0100

    feat: have tmux be as out-of-the-box as possible

--- profiles/dev/multiplexer.nix
+++ profiles/dev/multiplexer.nix
@@ -47,18 +47,10 @@
           in
           {
             enable = true;
-            disableConfirmationPrompt = true;
             escapeTime = 0;
             historyLimit = 50000;
-            keyMode = "vi";
             terminal = "tmux-256color";
-            extraConfig = ''
-              set -g display-time 4000
-              set -g renumber-windows on
-              set -g status-interval 60
-              set -g status-right ""
-              ${popupBindings}
-            '';
+            extraConfig = popupBindings;
           };
       }
     ];
