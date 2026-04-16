87ab4d2ab18900ffc1ce207ad457e8d22fa79451
Kai Chevannes <chevannes.kai@gmail.com>
2026-04-16 18:49:22 +0100

    chore: remove duplicated code

--- profiles/dev/shell.nix
+++ profiles/dev/shell.nix
@@ -40,20 +40,6 @@ lib.mkIf config.profiles.dev.enable {
         shellAliases = {
           ls = "eza";
         };
-
-        initContent = ''
-          eval "$(starship init zsh)"
-
-          TRANSIENT_PROMPT="''${PROMPT// prompt / prompt --profile transient }"
-          TRANSIENT_RPROMPT="''${PROMPT// prompt / prompt --profile rtransient }"
-
-          autoload -Uz add-zle-hook-widget
-          add-zle-hook-widget zle-line-finish transient-prompt
-
-          function transient-prompt() {
-              PROMPT="$TRANSIENT_PROMPT" RPROMPT="$TRANSIENT_RPROMPT" zle .reset-prompt
-          }
-        '';
       };
     }
   ];
