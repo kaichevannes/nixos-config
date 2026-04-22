0a94c348bc22e39cc7bf27e0faf39ee253e13798
Kai Chevannes <chevannes.kai@gmail.com>
2026-04-22 19:44:55 +0100

    fix: reaper detects audio device

--- profiles/desktop/music/default.nix
+++ profiles/desktop/music/default.nix
@@ -11,14 +11,23 @@
 
   config = lib.mkIf config.profiles.desktop.music.enable {
     modules.secrets.enable = true;
-    modules.secrets.homeFiles = [
-      ".config/REAPER/reaper-license.rk"
-    ];
+    modules.secrets.homeFiles = [ ".config/REAPER/reaper-license.rk" ];
+    modules.persist.homeDirectories = [ ".config/REAPER" ];
 
     services.pipewire.jack.enable = true;
 
-    environment.systemPackages = with pkgs; [
-      reaper
+    environment.systemPackages = [
+      (pkgs.symlinkJoin {
+        name = "reaper-jackwrapped";
+        paths = [ pkgs.reaper ];
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          for b in "$out/bin/"*; do
+            wrapProgram "$b" \
+              --prefix LD_LIBRARY_PATH : "${pkgs.pipewire.jack}/lib"
+          done
+        '';
+      })
     ];
   };
 }
