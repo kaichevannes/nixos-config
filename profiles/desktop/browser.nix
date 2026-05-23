b2665b19ca3f5785e93e0b049a882de05cf7bf6b
Kai Chevannes <chevannes.kai@gmail.com>
2026-05-23 19:20:15 +0100

    feat: update from .mozilla to .config/mozilla

--- profiles/desktop/browser.nix
+++ profiles/desktop/browser.nix
@@ -1,40 +1,44 @@
 { config, lib, ... }:
 lib.mkIf config.profiles.desktop.enable {
   modules.persist.homeDirectories = [
-    ".mozilla"
+    ".config/mozilla"
     "Downloads"
   ];
 
   home-manager.sharedModules = [
-    {
-      programs.chromium.enable = true;
+    (
+      { config, ... }:
+      {
+        programs.chromium.enable = true;
 
-      programs.firefox = {
-        enable = true;
-        profiles.default.id = 0;
-        profiles.llm = {
-          id = 1;
-          settings = {
-            "browser.tabs.inTitlebar" = 1;
-            "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        programs.firefox = {
+          enable = true;
+          configPath = "${config.xdg.configHome}/mozilla/firefox";
+          profiles.default.id = 0;
+          profiles.llm = {
+            id = 1;
+            settings = {
+              "browser.tabs.inTitlebar" = 1;
+              "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+            };
+            userChrome = ''
+              #TabsToolbar { visibility: collapse !important; }
+              #nav-bar { visibility: collapse !important; }
+            '';
           };
-          userChrome = ''
-            #TabsToolbar { visibility: collapse !important; }
-            #nav-bar { visibility: collapse !important; }
-          '';
-        };
-        profiles.whatsapp = {
-          id = 2;
-          settings = {
-            "browser.tabs.inTitlebar" = 1;
-            "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          profiles.whatsapp = {
+            id = 2;
+            settings = {
+              "browser.tabs.inTitlebar" = 1;
+              "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+            };
+            userChrome = ''
+              #TabsToolbar { visibility: collapse !important; }
+              #nav-bar { visibility: collapse !important; }
+            '';
           };
-          userChrome = ''
-            #TabsToolbar { visibility: collapse !important; }
-            #nav-bar { visibility: collapse !important; }
-          '';
         };
-      };
-    }
+      }
+    )
   ];
 }
